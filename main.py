8c59749c1578643f3dee32e7937ae25d3b7a7296
david <miner2.blablmgmail.co>
2021-12-30 23:35:10 +0100

    Test

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ if __name__ == '__main__':
     print(num_vectors_d.shape)
     dreieckdf=pd.DataFrame(num_vectors_d)
     dreieckdf_zeros= pd.DataFrame(np.zeros((1, 2500)))
+    pd.DataFrame(dreieckdf.to_csv("dvectors.csv"))
 
-    folder_path_K = glob.glob("/home/david/Downloads/drive-download-20211222T092238Z-001/Kreis")
+    folder_path_K = glob.glob("/home/david/Downloads/drive-download-20211222T092238Z-001/Kreis/*.png")
     cv_img_K = []
     for img in folder_path_K:
         n = cv2.imread(img,0)
@@ -34,9 +35,9 @@ if __name__ == '__main__':
     print(num_vectors_K.shape)
     kreisdf=pd.DataFrame(num_vectors_K)
     kreisdf_zeros= pd.DataFrame(np.zeros((1, 2500)))
-    pd.DataFrame(dreieckdf.to_csv("vectors.csv"))
+    pd.DataFrame(kreisdf.to_csv("kvectors.csv"))
     
-    folder_path_H = glob.glob("/home/david/Downloads/drive-download-20211222T092238Z-001/Herz")
+    folder_path_H = glob.glob("/home/david/Downloads/drive-download-20211222T092238Z-001/Herz/*.png")
     cv_img_H = []
     for img in folder_path_H:
         n = cv2.imread(img,0)
@@ -48,7 +49,22 @@ if __name__ == '__main__':
     num_vectors_H=np.asarray(vectors_first_H)
     np.reshape(num_vectors_H,-1)
     print(num_vectors_H.shape)
-    kreisdf=pd.DataFrame(num_vectors_H)
-    kreisdf_zeros= pd.DataFrame(np.zeros((1, 2500)))
-    pd.DataFrame(dreieckdf.to_csv("vectors.csv"))
-#enisenis
+    herzdf=pd.DataFrame(num_vectors_H)
+    herzdf_zeros= pd.DataFrame(np.zeros((1, 2500)))
+    pd.DataFrame(herzdf.to_csv("hvectors.csv"))
+
+    folder_path_Q = glob.glob("/home/david/Downloads/drive-download-20211222T092238Z-001/Quadrat/*.png")
+    cv_img_Q = []
+    for img in folder_path_Q:
+        n = cv2.imread(img, 0)
+        cv_img_Q.append(n)
+    vectors_first_Q = []
+    for i in cv_img_Q:
+        x = np.ravel(i)
+        vectors_first_Q.append(x)
+    num_vectors_Q = np.asarray(vectors_first_Q)
+    np.reshape(num_vectors_Q, -1)
+    print(num_vectors_Q.shape)
+    quaddf = pd.DataFrame(num_vectors_Q)
+    quaddf_zeros = pd.DataFrame(np.zeros((1, 2500)))
+    pd.DataFrame(quaddf.to_csv("qvectors.csv"))
